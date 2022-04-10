bf66c52dd07d283584141350d70dbc87ba5cd42a
Tal Berenstein <tal.berenstein@pm.me>
2022-04-10 23:10:51 +0200

    3d targeting

--- module3/main.js
+++ module3/main.js
@@ -1,10 +1,20 @@
+import { GLTFLoader } from "../applications/libs/three.js-r132/examples/jsm/loaders/GLTFLoader.js"
 const THREE = window.MINDAR.IMAGE.THREE;
 import { mockWithVideo } from '../applications/libs/camera-mock.js'
+import { loadGLTF } from "../applications/libs/loader.js"
 
+// const loadGLTF = (path) => {
+//     return new Promise((resolve, reject) => {
+//         const loader = new GLTFLoader();
+//         loader.load(path, (gltf) => {
+//             resolve(gltf);
+//         })
+//     })
+// }
 document.addEventListener('DOMContentLoaded', () => {
     const start = async () => {
 
-        mockWithVideo("http://localhost:8887/applications/assets/mock-videos/course-banner1.mp4");
+        // mockWithVideo("http://localhost:8887/applications/assets/mock-videos/course-banner1.mp4");
 
         // navigator.mediaDevices.getUserMedia = () => {
         //     return new Promise((resolve, reject) => {
@@ -19,17 +29,32 @@ document.addEventListener('DOMContentLoaded', () => {
         //     });
         // }
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+            // container: document.querySelector("#my-ar-container"),
             container: document.body,
-            imageTargetSrc: '../applications/assets/targets/course-banner.mind',
+            imageTargetSrc: '../applications/assets/targets/musicband.mind',
         });
         console.log(mindarThree)
 
         const { renderer, scene, camera } = mindarThree;
-        const geometry = new THREE.PlaneGeometry(1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x000ff, transparent: true, opacity: 0.5 });
-        const plane = new THREE.Mesh(geometry, material);
+        // const geometry = new THREE.PlaneGeometry(1, 1);
+        // const material = new THREE.MeshBasicMaterial({ color: 0x000ff, transparent: true, opacity: 0.5 });
+        // const plane = new THREE.Mesh(geometry, material);
+
+        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        scene.add(light);
         const anchor = mindarThree.addAnchor(0);
-        anchor.group.add(plane); // THREE.Group
+
+        const gltf = await loadGLTF("../applications/assets/models/musicband-raccoon/scene.gltf")
+        gltf.scene.scale.set(0.1, 0.1, 0.1);
+        gltf.scene.position.set(0, -0.4, 0);
+        anchor.group.add(gltf.scene);
+        // loader.load("../applications/assets/models/musicband-raccoon/scene.gltf", (gltf) => {
+        //     //gltf.scene: THREE.Group
+        //     gltf.scene.scale.set(0.1, 0.1, 0.1);
+        //     gltf.scene.position.set(0, -0.4, 0);
+        //     anchor.group.add(gltf.scene);
+        // })
+        // anchor.group.add(plane); // THREE.Group
 
         await mindarThree.start();
 
